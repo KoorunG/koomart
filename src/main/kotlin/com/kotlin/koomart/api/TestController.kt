d0c9d53a21772e17f496d8e00e561a9f4572d370
KoorunGG <onlyzion10@naver.com>
2024-01-14 20:17:24 +0900

    feat: api 연결 확인용 TestController 생성

---
+++ src/main/kotlin/com/kotlin/koomart/api/TestController.kt
@@ -0,0 +1,14 @@
+package com.kotlin.koomart.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hello")
+class TestController {
+
+    @GetMapping("/{value}")
+    fun hello(@PathVariable value: String) = "Hello, $value"
+}
