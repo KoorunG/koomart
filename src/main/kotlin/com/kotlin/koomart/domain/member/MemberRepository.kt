34422a7addec4912514760de862ca3fdfde0908c
KoorunGG <onlyzion10@naver.com>
2024-01-16 22:28:24 +0900

    refactor: deleteAll 메소드를 deleteAllInBatch로 변경

--- src/main/kotlin/com/kotlin/koomart/domain/member/MemberRepository.kt
+++ src/main/kotlin/com/kotlin/koomart/domain/member/MemberRepository.kt
@@ -8,4 +8,7 @@ interface MemberRepository: Repository<Member, UUID> {
     fun findById(uuid: UUID): Member?
     fun save(member: Member): Member
     fun deleteById(uuid: UUID)
+
+    // deleteAllInBatch를 사용하면 where절 없이 테이블에 대해 일괄삭제 쿼리를 날린다!
+    fun deleteAllInBatch()
 }
