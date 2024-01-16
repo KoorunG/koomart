f1b4dd23ee750e6851ae093628e69937381fb645
KoorunGG <onlyzion10@naver.com>
2024-01-16 22:27:38 +0900

    refactor: 테스트용 객체를 생성하는 MemberFactory 리팩토링

--- src/main/kotlin/com/kotlin/koomart/domain/member/Member.kt
+++ src/main/kotlin/com/kotlin/koomart/domain/member/Member.kt
@@ -1,6 +1,7 @@
 package com.kotlin.koomart.domain.member
 
 import com.kotlin.koomart.domain.common.PrimaryKeyEntity
+import io.github.serpro69.kfaker.faker
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import java.util.UUID
@@ -24,16 +25,22 @@ class Member(
     @Column(nullable = false)
     var password = password
         protected set
+}
 
-    // 테스트용 fixture
-    companion object {
-        fun fixture(
-            id: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000"),
-            loginId: String = "testid",
-            password: String = "testpw",
-            name: String = "테스트용"
-        ): Member {
-            return Member(_id = id, loginId = loginId, password = password, name = name)
-        }
+// 테스트용 멤버를 생성하는 Factory
+object MemberFactory {
+    private val faker = faker {  }
+    fun fixture(
+        id: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000"),
+        loginId: String = "testid",
+        password: String = "testpw",
+        name: String = "테스트용"
+    ): Member {
+        return Member(_id = id, loginId = loginId, password = password, name = name)
     }
+    fun fakeMember() = Member(
+        loginId = faker.string.bothify("${faker.name.firstName().lowercase()}_####", false),
+        name = faker.name.name(),
+        password = faker.string.bothify("#?#?#?###???")
+    )
 }
